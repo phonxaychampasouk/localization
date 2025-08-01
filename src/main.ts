0b6a8a80e4046491536dea5f9ff75bfdbf75d75b
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2025-08-01 03:29:38 -0400

    Add i18n to global registry

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+    locale: 'en',
+    messages:{}
+})
+const app = createApp(App);
+app.use(i18n);
+app.mount('#app');
